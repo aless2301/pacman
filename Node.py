7e9439bd09877aac21db7df62481983a17e3a775
marianoC31 <mariano05055@gmail.com>
2026-04-25 20:29:57 -0600

    hola_trabjando

--- Node.py
+++ Node.py
@@ -2,9 +2,9 @@ class Node:
     def __init__(self, f1_pos, f2_pos, dirs, parent=None):
         self.f1_x, self.f1_y = f1_pos
         self.f2_x, self.f2_y = f2_pos
-        self.dirs = dirs # (dir_fantasma1, dir_fantasma2)
+        self.dirs = dirs 
         self.parent = parent
         
-        self.g = 0  # Costo del camino
-        self.h = 0  # Heurística cooperativa
-        self.f = 0  # f = g + h
+        self.g = 0  
+        self.h = 0 
+        self.f = 0  
